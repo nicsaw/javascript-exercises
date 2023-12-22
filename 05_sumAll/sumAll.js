b673be2db0be60dc67031c8000fccc057add95c5
SAW <99191294+nicsaw@users.noreply.github.com>
2023-12-22 23:20:28 +0700

    Completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const ERROR = "ERROR";
 
+const sumAll = function(minNum, maxNum) {
+  if (typeof minNum !== "number" || typeof maxNum !== "number") return ERROR;
+  if (minNum < 0 || maxNum < 0) return ERROR;
+  if (minNum > maxNum) {
+    const temp = minNum;
+    minNum = maxNum;
+    maxNum = temp;
+  }
+
+  let sum = 0;
+  for (let i = minNum; i <= maxNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
