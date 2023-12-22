27faf668efddcfa8d3c35162cd06f721c564c467
SAW <99191294+nicsaw@users.noreply.github.com>
2023-12-22 23:27:31 +0700

    Completed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const roundOneDP = (num) => Math.round(num * 10) / 10;
+
+const convertToCelsius = function(temperature) {
+  return roundOneDP((temperature - 32) * 5 / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  return roundOneDP(temperature * 9 / 5 + 32);
 };
 
 // Do not edit below this line
