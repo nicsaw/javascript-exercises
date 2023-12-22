395deec29883622281f900153dfa6b60b9209281
SAW <99191294+nicsaw@users.noreply.github.com>
2023-12-22 23:12:13 +0700

    Completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...elemsToRemove) {
+  return arr.filter(elem => !elemsToRemove.includes(elem));
 };
 
 // Do not edit below this line
